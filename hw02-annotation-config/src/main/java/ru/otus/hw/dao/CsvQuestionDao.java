8675ff85b8da736a7e0467f2b2087679cb47cfa4
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-04-09 23:36:25 +0300

    added annotation; refined the logic of displaying questions with answers and processing user input

--- hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,14 +1,25 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 
-import java.util.ArrayList;
 import java.util.List;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+@Repository
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
+    public static final int ROW_OFFSET = 1;
+    public static final char QUESTION_SEPARATOR = ';';
     private final TestFileNameProvider fileNameProvider;
 
     @Override
@@ -18,6 +29,19 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
+            List<QuestionDto> questionDTOs = new CsvToBeanBuilder(new InputStreamReader(resourceAsStream))
+                .withType(QuestionDto.class)
+                .withSkipLines(ROW_OFFSET)
+                .withSeparator(QUESTION_SEPARATOR)
+                .build()
+                .parse();
+
+            return questionDTOs.stream()
+                .map(QuestionDto::toDomainObject)
+                .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new QuestionReadException("Fail while reading csv file", e);
+        }
     }
 }
--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.otus.hw.service;
 
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+@Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
@@ -19,9 +22,15 @@ public class TestServiceImpl implements TestService {
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        for (var question : questions) {
+            ioService.printLine(question.text());
+
+            IntStream.range(0, question.answers().size())
+                .forEach(value -> ioService.printFormattedLine("\t%s. %s", value + 1, question.answers().get(value).text()));
+
+            int answerNumber = ioService.readIntForRange(1, question.answers().size(),
+                "The entered answer number does not correspond to the proposed answers. Enter a valid number");
+            testResult.applyAnswer(question, question.answers().get(answerNumber - 1).isCorrect());
         }
         return testResult;
     }
