9b932383b5ed3660f3411c362db16737834fe13b
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-04-15 14:47:21 +0300

    cosmetic

--- hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
@@ -3,12 +3,12 @@ package ru.otus.hw;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.TestRunnerService;
 
 @PropertySource(value = {"classpath:application.properties"})
 @ComponentScan
-//@Configuration
 public class Application {
     public static void main(String[] args) {
 
