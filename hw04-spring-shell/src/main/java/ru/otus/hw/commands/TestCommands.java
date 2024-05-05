721ebea1ed5e099e2664ea6c1478881b9ec7cd87
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-05-05 15:56:18 +0300

    added localized command messages

--- hw04-spring-shell/src/main/java/ru/otus/hw/commands/TestCommands.java
+++ hw04-spring-shell/src/main/java/ru/otus/hw/commands/TestCommands.java
@@ -1,7 +1,8 @@
 package ru.otus.hw.commands;
 
-import java.util.Objects;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.shell.Availability;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.standard.ShellComponent;
@@ -10,6 +11,7 @@ import org.springframework.shell.standard.ShellMethodAvailability;
 import org.springframework.shell.standard.ShellOption;
 import ru.otus.hw.domain.TestResult;
 import ru.otus.hw.security.StudentContext;
+import ru.otus.hw.service.LocalizedIOService;
 import ru.otus.hw.service.ResultService;
 import ru.otus.hw.service.TestRunnerService;
 
@@ -24,28 +26,31 @@ public class TestCommands {
 
     private final ResultService resultService;
 
+    private final LocalizedIOService localizedIOService;
+
     @ShellMethod(value = "Login", key = {"login", "l"})
-    public String login(@ShellOption(value = {"--first-name", "-fn"}) String firstName,
-                        @ShellOption(value = {"--last-name", "-ln"}) String lastName) {
+    public void login(@ShellOption(value = {"--first-name", "-fn"}) String firstName,
+                      @ShellOption(value = {"--last-name", "-ln"}) String lastName) {
         studentContext.login(firstName, lastName);
-        return String.format("You have successfully logged in as %s %s", firstName, lastName);
+        localizedIOService.printFormattedLineLocalized("TestCommands.successfully.logged", firstName, lastName);
     }
 
     @ShellMethod(value = "Run test", key = {"run_test", "rt"})
-    public String runTest() {
+    public void runTest() {
         testRunnerService.run();
-        return "The test is completed";
+        localizedIOService.printLineLocalized("TestCommands.test.completed");
     }
 
     @ShellMethod(value = "Show result", key = {"show_result", "sr"})
     public void showResult() {
-        TestResult result = resultService.getResult(studentContext.getStudent());
+        Set<TestResult> results = resultService.getResults(studentContext.getStudent());
 
-        if (Objects.isNull(result)) {
-            throw new RuntimeException("You haven't taken the test yet, please run the Run command first");
+        if (CollectionUtils.isEmpty(results)) {
+            localizedIOService.printLineLocalized("TestCommands.need.complete.test.before.show.result");
+            return;
         }
 
-        resultService.showResult(result);
+        results.forEach(resultService::showResult);
     }
 
     @ShellMethodAvailability({"run_test", "show_result"})
