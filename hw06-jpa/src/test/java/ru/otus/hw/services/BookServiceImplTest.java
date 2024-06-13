70c2eb3c55060bab192e3c92bf5da7075f75b1f8
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-06-13 17:30:54 +0300

    disable spring.shell.interactive

--- hw06-jpa/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
+++ hw06-jpa/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
@@ -24,7 +24,7 @@ import ru.otus.hw.dto.BookDTO;
 import ru.otus.hw.dto.CommentDTO;
 import ru.otus.hw.dto.GenreDTO;
 
-@SpringBootTest(properties = "spring.shell.interactive.enabled=false")
+@SpringBootTest
 @Transactional(propagation = Propagation.NEVER)
 @TestMethodOrder(OrderAnnotation.class)
 class BookServiceImplTest {
