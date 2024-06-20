0fa732eb17919f5d7d5f738321407cb504755542
Aleksandr Shakhoff <A.SHahov@ase-ec.ru>
2024-06-20 23:59:12 +0300

    Added use of Spring Data JPA

--- hw07-spring-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07-spring-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -2,13 +2,13 @@ package ru.otus.hw.repositories;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Comment;
 
-public interface CommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Optional<Comment> findById(long id);
-
-    Comment save(Comment comment);
 
     void deleteById(long id);
 
