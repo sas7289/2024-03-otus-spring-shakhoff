a02d5c9dff80fe19aa283be70a7313f07ff00558
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-06-25 17:48:07 +0300

    added dependencies and configs

--- hw08-spring-data-keyvalue/src/main/java/ru/otus/hw/Application.java
+++ hw08-spring-data-keyvalue/src/main/java/ru/otus/hw/Application.java
@@ -1,11 +1,13 @@
 package ru.otus.hw;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableMongock
 public class Application {
 
 	public static void main(String[] args) {
