a7b2dd3c61b9daaee87c48d5208e054c95fad2db
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-07-03 18:03:33 +0300

    draft

---
+++ hw09-spring-mvc/src/main/java/ru/otus/hw/commands/BookController.java
@@ -0,0 +1,82 @@
+package ru.otus.hw.commands;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.dto.AuthorDTO;
+import ru.otus.hw.dto.BookDTO;
+import ru.otus.hw.dto.GenreDTO;
+import ru.otus.hw.services.AuthorService;
+import ru.otus.hw.services.BookService;
+
+import java.util.Set;
+import ru.otus.hw.services.GenreService;
+
+@RequiredArgsConstructor
+@Controller
+public class BookController {
+
+    private final BookService bookService;
+
+    private final AuthorService authorService;
+
+    private final GenreService genreService;
+
+    private final BookConverter bookConverter;
+
+    @GetMapping("/books")
+    public String findAllBooks(Model model) {
+        List<BookDTO> books = bookService.findAll();
+        List<AuthorDTO> authors = authorService.findAll();
+        List<GenreDTO> genres = genreService.findAll();
+        model.addAttribute("books", books);
+        model.addAttribute("authors", authors);
+        model.addAttribute("genres", genres);
+        return "list";
+    }
+
+    @GetMapping("/books/{id}")
+    public String findBookById(@PathVariable("id") long id, Model model) {
+        BookDTO book = bookService.findById(id)
+            .orElseThrow(() -> new RuntimeException("ALARM!"));
+        model.addAttribute("book", book);
+        return "book";
+    }
+
+    @GetMapping("/books/edit/{id}")
+    public String editBook(@PathVariable("id") long id, Model model) {
+        BookDTO book = bookService.findById(id)
+            .orElseThrow(() -> new RuntimeException("ALARM!"));
+        List<AuthorDTO> authors = authorService.findAll();
+        List<GenreDTO> genres = genreService.findAll();
+        model.addAttribute("book", book);
+        model.addAttribute("authors", authors);
+        model.addAttribute("genres", genres);
+        return "edit";
+    }
+
+    @PostMapping("/books")
+    public String insertBook(@RequestParam String title, @RequestParam long authorId,
+                             @RequestParam Set<Long> genresIds) {
+        var savedBook = bookService.insert(title, authorId, genresIds);
+        return "redirect:/books";
+    }
+
+    @PostMapping("/books/update")
+    public String updateBook(@RequestParam long id, @RequestParam String title, @RequestParam long authorId, @RequestParam Set<Long> genresIds) {
+        var savedBook = bookService.update(id, title, authorId, genresIds);
+        return bookConverter.bookDtoToString(savedBook);
+    }
+
+    @GetMapping("books/delete/{id}")
+    public String deleteBook(@PathVariable("id") long id) {
+        bookService.deleteById(id);
+        return "redirect:/books";
+    }
+}
--- hw09-spring-mvc/src/main/java/ru/otus/hw/commands/GenreCommands.java
+++ hw09-spring-mvc/src/main/java/ru/otus/hw/commands/GenreCommands.java
@@ -1,22 +1,23 @@
 package ru.otus.hw.commands;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.otus.hw.converters.GenreConverter;
 import ru.otus.hw.services.GenreService;
 
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-@ShellComponent
+@Controller
 public class GenreCommands {
 
     private final GenreService genreService;
 
     private final GenreConverter genreConverter;
 
-    @ShellMethod(value = "Find all genres", key = "ag")
+//    @ShellMethod(value = "Find all genres", key = "ag")
+    @GetMapping("/genres")
     public String findAllGenres() {
         return genreService.findAll().stream()
                 .map(genreConverter::genreDtoToString)
