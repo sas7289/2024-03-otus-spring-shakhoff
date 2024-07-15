651faa48e708979459a8e923968f21586b7fc278
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-07-15 22:11:27 +0300

    improvements

--- hw10-ajax/src/main/java/ru/otus/hw/Application.java
+++ hw10-ajax/src/main/java/ru/otus/hw/Application.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	/**
+	 *
+	 * для запуска фронта:
+	 * 	1. выполнеить команду mvn package
+	 * 	2. запустить Spring проект
+	 *	3. запустить frontend, выполнив команду из корневой директории проекта в PowerShell
+	 *	cd hw10-ajax\src\main\frontend;ng serve
+	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
