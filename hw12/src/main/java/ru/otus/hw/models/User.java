132d5eb0975749fb729f98a6b938fd781265eff8
Aleksandr Shakhov <A.SHahov@ase-ec.ru>
2024-07-30 23:18:34 +0300

    cosmetic

--- hw12/src/main/java/ru/otus/hw/models/User.java
+++ hw12/src/main/java/ru/otus/hw/models/User.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.Collection;
 import java.util.List;
--- hw12/src/main/java/ru/otus/hw/security/CustomUserDetailService.java
+++ hw12/src/main/java/ru/otus/hw/security/CustomUserDetailService.java
@@ -1,8 +1,6 @@
 package ru.otus.hw.security;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,18 +9,19 @@ import ru.otus.hw.models.User;
 import ru.otus.hw.repositories.UserRepository;
 
 @Service
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByName(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found by name: " + username));
 
-        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), user.getAuthorities());
+        return new org.springframework.security.core.userdetails.User(
+            user.getName(),
+            user.getPassword(),
+            user.getAuthorities());
     }
 }
